dfb936f983569a90098c627b32327c2b0bfad068
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2022-11-25 23:07:01 +0700

    test: update tests

---
+++ test/test.js
@@ -0,0 +1,14 @@
+function do_something(x) {
+    // This is a sample comment
+    if (x == 0) {
+        return 0
+    }
+    else if (x + 4 == 1) {
+        if (true) {
+            return 3
+        } else {
+            return 2
+        }
+    }
+}
+  
